92e1a5e9c57461ba446e73ef5ffafabefa611e13
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-01-18 17:56:32 -0300

    feat: adiciona layout 'Default' globalmente

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,11 +1,22 @@
 import { Box } from "native-base";
 import type { WithChildren } from "@global-types/WithChildren";
 
-type DefaultProps = Pick<Parameters<typeof Box>[0], "background"> & {};
+type DefaultProps = Omit<
+	Parameters<typeof Box>[0],
+	"background" | "flex" | "safeArea"
+> & {
+	hasDefaultSpacings?: boolean;
+};
 
 export const Default = (props: WithChildren<DefaultProps>) => {
+	const { hasDefaultSpacings = true, ...rest } = props;
+	const paddings = {
+		px: hasDefaultSpacings ? "24px" : 0,
+		pt: hasDefaultSpacings ? "8px" : 0,
+	};
+
 	return (
-		<Box background={props.background || "dark.900"} flex={1} safeArea>
+		<Box background="dark.900" flex={1} safeArea {...paddings} {...rest}>
 			{props.children}
 		</Box>
 	);
