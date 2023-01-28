2f84722f8f1f36f6ec2fe7afaae2a336a1b08359
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-01-28 19:23:18 -0300

    fix: arruma tipagem

--- src/components/Carousel/@types/NavigationDefaultProps.d.ts
+++ src/components/Carousel/@types/NavigationDefaultProps.d.ts
@@ -3,5 +3,6 @@ import type { AccessibilityValue } from "react-native";
 export type NavigationDefaultProps<RestProps extends {}> = {
 	onPress: () => void;
 	accessibilityValue: AccessibilityValue;
+	accessibilityHint: string;
 	isActive: boolean;
 } & RestProps;
