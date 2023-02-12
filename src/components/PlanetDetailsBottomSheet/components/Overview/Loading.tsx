81a132a310cd813363c503f8eeeb776a77a6690e
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-02-12 17:44:07 -0300

    fix: arruma cores do componente 'Loading' de 'Overview'

--- src/components/PlanetDetailsBottomSheet/components/Overview/Loading.tsx
+++ src/components/PlanetDetailsBottomSheet/components/Overview/Loading.tsx
@@ -2,8 +2,20 @@ import { Skeleton, VStack, HStack } from "native-base";
 
 const InfoGroupLoading = () => (
 	<VStack space="2">
-		<Skeleton h="4" w="32" rounded="full" />
-		<Skeleton h="3" w="24" rounded="full" />
+		<Skeleton
+			h="4"
+			w="32"
+			rounded="full"
+			startColor="dark.600"
+			endColor="dark.800"
+		/>
+		<Skeleton
+			h="3"
+			w="24"
+			rounded="full"
+			startColor="dark.600"
+			endColor="dark.800"
+		/>
 	</VStack>
 );
 
@@ -11,8 +23,14 @@ export const Loading = () => {
 	return (
 		<VStack space="32px">
 			<VStack space="6">
-				<Skeleton h="5" w="48" rounded="full" />
-				<Skeleton.Text />
+				<Skeleton
+					h="5"
+					w="48"
+					rounded="full"
+					startColor="dark.600"
+					endColor="dark.800"
+				/>
+				<Skeleton.Text startColor="dark.600" endColor="dark.800" />
 			</VStack>
 			<HStack alignItems="center" flexWrap="wrap" space="66px">
 				<VStack space="24px">
